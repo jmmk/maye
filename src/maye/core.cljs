2d396583b881ed59aa20c1bea13f61288c856d22
Michael McLellan <jmikem825@gmail.com>
2015-11-25 19:19:31 -0500

    Re-export functions in core namespace

--- src/maye/core.cljs
+++ src/maye/core.cljs
@@ -1,1 +1,26 @@
-(ns maye.core)
+(ns maye.core
+  (:require [maye.component :as component]
+            [maye.entity :as entity]
+            [maye.system :as system]
+            [maye.state :as state]))
+
+;;-------Component--------
+(defn new-component component/new-component)
+
+;;-------Entity----------
+(defn new-entity entity/new-entity)
+(defn assoc-components entity/assoc-components)
+(defn assoc-component entity/assoc-component)
+(defn dissoc-components entity/dissoc-components)
+(defn dissoc-component entity/dissoc-component)
+(defn contains-components? entity/contains-components?)
+
+;;------System-----------
+(defn new-system system/new-system)
+(defn frame-period system/frame-period)
+
+;;------State---------
+(defn new-state state/new-state)
+(defn add-systems state/add-systems)
+(defn add-entities state/add-entities)
+(defn update-state state/update-state)
